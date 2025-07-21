ef610e8e8646cafae878de4563f36f1c29159082
hazem sghaier <h.sghaier@proxiad.com>
2025-07-21 19:19:04 +0200

    first commit

---
+++ src/test/java/org/hazem/sse/projectv2/Projectv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.hazem.sse.projectv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projectv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
